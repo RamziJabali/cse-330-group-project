35d2df0c9a72d6f764529b60595200e6089a641a
Kim P. Finical <79591944+FinicalKim@users.noreply.github.com>
2023-03-13 12:50:26 -0700

    Update producer_consumer.c

--- Project2/producer_consumer.c
+++ Project2/producer_consumer.c
@@ -20,6 +20,7 @@ static int buff_size = 0;
 static int p = 0;
 static int c = 0;
 
+	// takes input arguments and assigns to respective variables
 module_param(buff_size, int, 0);
 module_param(p, int, 0);
 module_param(c, int, 0);
@@ -50,19 +51,21 @@ struct semaphore mutex;
 // Define a global int to track total task time
 long total_time;
 
-struct task_struct* p;
-size_t process_counter = 0;
-for_each_process(p) {
-++process_counter;
-}
+struct consumer{
+	struct task_struct *consumer_thread;
+	struct consumer_node *next;
+} consumer;
 
+// data struct declaration
+struct data{
+	int capacity;
+	struct task_node *head;
+} data;
 
 // Linked List declaration 
-struct task_node
-{
+struct task_node{
 	struct task_struct *task;
 	struct task_node *next;
-
 } task_list;
 
 static inline void sema_init(struct semaphore *sem, int val) // a function to initialize a semaphore structure
@@ -72,50 +75,101 @@ void down_interruptible(struct semaphore *sem) // acquire a lock
 void up(struct semaphore *sem) // release a lock
 
 struct semaphore name; // Defines a semaphore with a given name
-static inline void sema_init(struct semaphore *sem, int val) // a
-function to initialize a semaphore structure
+static inline void sema_init(struct semaphore *sem, int val) // a function to initialize a semaphore structure
 void down_interruptible(struct semaphore *sem) // acquire a lock
 void up(struct semaphore *sem) // release a lock
 
 
 ///////////////////////////////////
-
-static int sema_init(void)
+	// needs work
+static int __init sema_init(void)
 {
 
-      // Initialize semaphores
-    sema_init(&empty, buff_size);
-    sema_init(&full, 0); 
-    sema_init(&mutex, 1);
+    	  // Initialize semaphores
+    	sema_init(&empty, buff_size);
+    	sema_init(&full, 0); 
+    	sema_init(&mutex, 1);
   
-      // set the initial values to NULL in the linked list
-    task_list.task = NULL;
-    task_list.next = NULL;
+    	  // set the initial values to NULL in the linked list
+    	task_list.task = NULL;
+    	task_list.next = NULL;
+	
+	data.capacity = buff_size;
+	data.head = &task_list;
+	
+
+return 0;
   
 }
 
 // Semaphore Definition
 sema_init(&empty, 5); // init the semaphore as 5
-// if the thread works in an infinite loop, this is how it knows when
-to stop. Check (4) module_exit for more information.
-while (!kthread_should_stop())
-{
-if (down_interruptible(&empty))
-/* down_interruptible allows the calling thread to be
-interrupted even if it has not acquired the semaphore that
-it has been waiting for */
-break; // exit
-// Critical section
-up(empty)
-// signal the semaphore
-}
+// if the thread works in an infinite loop, this is how it knows when to stop. Check (4) module_exit for more information.
 
+	// needs work
 static int producer_func(void *arg)
 {
+	struct task_struct* p;
+	size_t process_counter = 0;
+	for_each_process(p){
+		if(){
+		
+			
+			++process_counter;
+		}
+	}
+	
+	
+	return 0;
 }
   
 static int consumer_func(void *arg)
 {
+	
+	while (!kthread_should_stop())
+	{
+		if (down_interruptible(&full))
+		{
+			break;
+		}
+		else if (down_interruptible(&mutex))
+		{
+			break;
+		}
+			// if the consumer attempts to take from an empty buffer
+		if(data.head == NULL)
+		{
+          	  printk(KERN_INFO "error");
+        	}else
+		{
+		struct task_node *temp;
+		temp = data.head;
+		
+		// traverse temp linked list
+		if(temp->next != NULL){
+			data.head = data.head->next;
+		}else{
+			printk(KERN_INFO "" )
+		}
+			
+		long current_time = ktime_get_ns();
+		long start_time = temp->task->start_time;
+		long task_time = current_time - start_time;
+		u64 base = task_time/1000000000;
+		int hours = base/3600;
+		int minutes = (base - (hours*3600))/60;
+		int seconds = base - (hours*3600) - (minutes*60);
+		total_time += task_time;
+		
+		printk()
+			
+				  // increment semaphores
+	        up(&mutex);
+	
+        	up(&empty);
+		}
+	
+	return 0;
 }
 
 task_struct *task;
@@ -127,6 +181,10 @@ ktime_get_ns() // current time in nanoseconds.
 kthread_stop(struct task_struct *k)
 // when kthread_stop() is called, this function will return true
 kthread_should_stop(void)
+	
+static void __exit sema_exit(){
+
+}
   
-module_init();
-module_exit();
+module_init(sema_init);
+module_exit(sema_exit);
